72f90d4d8ef5a2cc8bcd88fac9a1fa644203781f
kapsner <lorenz.kapsner@gmail.com>
2022-10-27 14:30:03 +0200

    refactor!: rename to mlh_subset

--- R/mlh_subset.R
+++ R/mlh_subset.R
@@ -1,4 +1,4 @@
-#' @title mlh_format_xy
+#' @title mlh_subset
 #'
 #' @description Machine learning helper function to select a subset from a
 #'   data matrix or a response vector.
@@ -7,20 +7,20 @@
 #' @param ids An integer vector specifying the indices that should be selected
 #'   from the object.
 #'
-#' @return Returns a subset of object.
+#' @return Returns the specified subset of the object.
 
 #' @examples
 #' data("iris")
-#' mlh_format_xy(iris, c(1:30))
-#' mlh_format_xy(iris[, 5], c(1:30))
+#' mlh_subset(iris, c(1:30))
+#' mlh_subset(iris[, 5], c(1:30))
 #' @export
 #'
-mlh_format_xy <- function(object, ids) {
+mlh_subset <- function(object, ids) {
   stopifnot(is.integer(ids))
   if (is.null(dim(object)) && is.atomic(object)) {
     return(object[ids])
   } else {
-    stopifnot(is.matrix(object) || is.data.frame(object))
+    stopifnot(inherits(object, "matrix") || inherits(object, "data.frame"))
     return(object[ids, ])
   }
 }
