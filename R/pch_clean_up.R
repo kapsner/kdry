eb261ae7ba37a9ff100c54ed087efa6ac9603009
kapsner <lorenz.kapsner@gmail.com>
2022-11-13 18:45:45 +0100

    refactor: removed unnecessary pkg ns check 2

--- R/pch_clean_up.R
+++ R/pch_clean_up.R
@@ -19,24 +19,6 @@
 #
 pch_clean_up <- function(cl) {
   stopifnot(inherits(cl, c("SOCKcluster", "cluster")))
-  if (!requireNamespace("parallel", quietly = TRUE)) {
-    stop(
-      paste0(
-        "Package \"parallel\" must be installed to use ",
-        "\"pch_clean_up\"."
-      ),
-      call. = FALSE
-    )
-  }
-  if (!requireNamespace("foreach", quietly = TRUE)) {
-    stop(
-      paste0(
-        "Package \"foreach\" must be installed to use ",
-        "\"pch_clean_up\"."
-      ),
-      call. = FALSE
-    )
-  }
   parallel::stopCluster(cl)
   foreach::registerDoSEQ()
   invisible()
